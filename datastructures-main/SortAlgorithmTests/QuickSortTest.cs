767f9f6786e3a69b17a669fc20dfce36f16c4ff0
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-01-20 12:31:39 +0100

    new sort pattern

---
+++ datastructures-main/SortAlgorithmTests/QuickSortTest.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using Datastructures;
+using NUnit.Framework;
+using SortingAlgorithms;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SortAlgorithmTest
+{
+    public class QuicksortTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestStrategyPatternQuickSort_InsertNumbers1865_ReturnRightOrder()
+        {
+            var linkedList = new SingleLinkedList();
+            linkedList.SetSortStrategy(new QuickSort());
+            linkedList.insertLast(2);
+            linkedList.insertLast(7);
+            linkedList.insertLast(5);
+            linkedList.insertLast(6);
+            linkedList.Sort();
+            Assert.AreEqual(linkedList.head.data, 2);
+            Assert.AreEqual(linkedList.head.next.data, 5);
+            Assert.AreEqual(linkedList.head.next.next.data, 6);
+            Assert.AreEqual(linkedList.head.next.next.next.data, 7);
+        }
+    }
+}
---
+++ datastructures-main/SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class QuickSort : SortStrategy
+    {
+        public override void Sort(IMyList list)
+        {
+            static Node quickSort(Node head, Node end)
+            {
+                if (head != end)
+                {
+                    Node temp = partition(head, end);
+                    quickSort(head, temp);
+                    quickSort(temp.next, end);
+                }
+                return head;
+            }
+
+            static Node partition(Node head, Node end)
+            {
+                int pivot = end.data;
+                Node i = head;
+                for (Node j = head; j != end; j = j.next)
+                {
+                    if (j.data <= pivot)
+                    {
+                        i = (i == null) ? head : i.next;
+                        int temp = i.data;
+                        i.data = j.data;
+                        j.data = temp;
+                    }
+                }
+                i = (i == null) ? head : i.next;
+                int temp2 = i.data;
+                i.data = end.data;
+                end.data = temp2;
+                return i;
+            }
+
+
+        }
+    }
+}
